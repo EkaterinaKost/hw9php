837fba2262649cfc843b9c3732a89132d3daf2ce
ekaterina <kostcat-84@mail.ru>
2024-08-12 21:16:21 +0500

    homework9

---
+++ src/Domain/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Geekbrains\Application1\Domain\Controllers;
+use Geekbrains\Application1\Application\Render;
+
+class PageController {
+
+    public function actionIndex() {
+        $render = new Render();
+        
+        return $render->renderPage('page-index.tpl', ['title' => 'Главная страница']);
+    }
+
+    public function actionTime(): string 
+    {
+      return json_encode(date('d-m-Y H:i:s'))  ;
+    }
+}
